3572f4f8e106fe4c9c61f62d4f823c4e07a547df
developmentd420 <development.d420@gmail.com>
2023-11-21 06:47:28 +0600

    fix send message problem

--- frontend/constants/config.js
+++ frontend/constants/config.js
@@ -1,5 +1,5 @@
-// const SERVER_API_URL = "https://task-management-app-backend.vercel.app";
-const SERVER_API_URL = "http://localhost:8000";
+const SERVER_API_URL = "https://task-management-app-backend.vercel.app";
+// const SERVER_API_URL = "http://localhost:8000";
 
 export default {
   SERVER_API_URL,
